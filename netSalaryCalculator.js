614d11f1a43e0d65a7cb5ff8bc546ddf399f4313
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-10-13 19:22:46 +0300

    Completed task

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -1,55 +1,148 @@
-var basicSalary = parseInt(prompt("Enter your basic salary:"));
-var benefits = parseInt(prompt("Enter the value of benefits:"));
+let totalBasicSalary = parseFloat(prompt("enter basic salary amount"));
+let totalBenefits = parseFloat(prompt("enter benefits amount"));
 
+const calculateGrossSalary = (basicSalary, benefits) => {
+  return basicSalary + benefits;
+};
 
-var taxRate;
-if (basicSalary <= 24000) {
-  taxRate = 10;
-} else if (basicSalary <= 32333) {
-  taxRate = 15;
-} else if (basicSalary <= 41000) {
-  taxRate = 20;
-} else if (basicSalary <= 89167) {
-  taxRate = 25;
-} else {
-  taxRate = 30;
-}
-var payee = (basicSalary + benefits) * (taxRate / 100);
+const calculatePaye = (basicSalary, benefits) => {
+  let totalTaxableIncome = calculateGrossSalary(basicSalary, benefits);
+  let totalTax = 0;
+  let personalRelief = 2400;
+  let netTax = 0;
 
+  if (totalTaxableIncome <= 24000) {
+    let firstBracket = totalTaxableIncome * 0.1;
+    totalTax = firstBracket;
+    netTax = totalTax - personalRelief;
 
-var nhifDeductions;
-if (basicSalary <= 5999) {
-  nhifDeductions = 150;
-} else if (basicSalary <= 7999) {
-  nhifDeductions = 300;
-} else if (basicSalary <= 11999) {
-  nhifDeductions = 400;
-} else if (basicSalary <= 14999) {
-  nhifDeductions = 500;
-} else if (basicSalary <= 19999) {
-  nhifDeductions = 600;
-} else if (basicSalary <= 24999) {
-  nhifDeductions = 750;
-} else if (basicSalary <= 29999) {
-  nhifDeductions = 850;
-} else if (basicSalary <= 34999) {
-  nhifDeductions = 900;
-} else if (basicSalary <= 39999) {
-  nhifDeductions = 950;
-} else if (basicSalary <= 44999) {
-  nhifDeductions = 1000;
-} else if (basicSalary <= 49999) {
-  nhifDeductions = 1100;
-} else if (basicSalary <= 59999) {
-  nhifDeductions = 1200;
-} else if (basicSalary <= 69999) {
-  nhifDeductions = 1300;
-} else if (basicSalary <= 79999) {
-  nhifDeductions = 1400;
-} else if (basicSalary <= 89999) {
-  nhifDeductions = 1500;
-} else if (basicSalary <= 99999) {
-  nhifDeductions = 1600;
-} else if (basicSalary <= 109999) {
-  nhifDeductions = 1700;
-}
+    return netTax;
+  } else if (totalTaxableIncome <= 32333) {
+    let firstBracket = 24000 * 0.1;
+    let secondBracket = (totalTaxableIncome - 24000) * 0.25;
+    totalTax = firstBracket + secondBracket;
+    netTax = totalTax - personalRelief;
+
+    return netTax;
+  } else if (totalTaxableIncome <= 500000) {
+    let firstBracket = 24000 * 0.1;
+    let secondBracket = (32333 - 24000) * 0.25;
+    let thirdBracket = (totalTaxableIncome - 32333) * 0.3;
+    totalTax = firstBracket + secondBracket + thirdBracket;
+    netTax = totalTax - personalRelief;
+
+    return netTax;
+  } else if (totalTaxableIncome <= 800000) {
+    let firstBracket = 24000 * 0.1;
+    let secondBracket = (32333 - 24000) * 0.25;
+    let thirdBracket = (500000 - 32333) * 0.3;
+    let fourthBracket = (totalTaxableIncome - 500000) * 0.325;
+    totalTax = firstBracket + secondBracket + thirdBracket + fourthBracket;
+    netTax = totalTax - personalRelief;
+
+    return netTax;
+  } else {
+    let firstBracket = 24000 * 0.1;
+    let secondBracket = (32333 - 24000) * 0.25;
+    let thirdBracket = (500000 - 32333) * 0.3;
+    let fourthBracket = (800000 - 500000) * 0.325;
+    let fifthBracket = (totalTaxableIncome - 800000) * 0.35;
+    totalTax =
+      firstBracket +
+      secondBracket +
+      thirdBracket +
+      fourthBracket +
+      fifthBracket;
+    netTax = totalTax - personalRelief;
+
+    return netTax;
+  }
+};
+
+const calculateNhif = (basicSalary, benefits) => {
+  let grossPay = calculateGrossSalary(basicSalary, benefits);
+  let nhif = 0;
+
+  if (grossPay <= 5999) {
+    nhif = 150;
+    return nhif;
+  } else if (grossPay <= 7999) {
+    nhif = 300;
+    return nhif;
+  } else if (grossPay <= 11999) {
+    nhif = 400;
+    return nhif;
+  } else if (grossPay <= 14999) {
+    nhif = 500;
+    return nhif;
+  } else if (grossPay <= 19999) {
+    nhif = 600;
+    return nhif;
+  } else if (grossPay <= 24999) {
+    nhif = 750;
+    return nhif;
+  } else if (grossPay <= 29999) {
+    nhif = 850;
+    return nhif;
+  } else if (grossPay <= 34999) {
+    nhif = 900;
+    return nhif;
+  } else if (grossPay <= 39999) {
+    nhif = 950;
+    return nhif;
+  } else if (grossPay <= 44999) {
+    nhif = 1000;
+    return nhif;
+  } else if (grossPay <= 49999) {
+    nhif = 1100;
+    return nhif;
+  } else if (grossPay <= 59999) {
+    nhif = 1200;
+    return nhif;
+  } else if (grossPay <= 69999) {
+    nhif = 1300;
+    return nhif;
+  } else if (grossPay <= 79999) {
+    nhif = 1400;
+    return nhif;
+  } else if (grossPay <= 89999) {
+    nhif = 1500;
+    return nhif;
+  } else if (grossPay <= 99999) {
+    nhif = 1600;
+    return nhif;
+  } else {
+    nhif = 1700;
+    return nhif;
+  }
+};
+
+const calculateNssf = (basicSalary, benefits) => {
+  let nssf = 0.05 * calculateGrossSalary(basicSalary, benefits);
+  return nssf;
+};
+
+const calculateNetSalary = (gross, paye, nhif, nssf) => {
+  let netSalary = gross - (paye + nhif + nssf);
+
+  return netSalary;
+};
+
+let totalGrossSalary = calculateGrossSalary(totalBasicSalary, totalBenefits);
+let totalPaye = calculatePaye(totalBasicSalary, totalBenefits);
+let totalNhif = calculateNhif(totalBasicSalary, totalBenefits);
+let totalNssf = calculateNssf(totalBasicSalary, totalBenefits);
+let totalNetSalary = calculateNetSalary(
+  totalGrossSalary,
+  totalPaye,
+  totalNhif,
+  totalNssf
+);
+
+console.log({
+  totalGrossSalary,
+  totalPaye,
+  totalNhif,
+  totalNssf,
+  totalNetSalary,
+});
